5c6f5150a7e938370428d9bede5bcf98065163bd
WebforData <kiraourui@gmail.com>
2022-03-19 11:59:26 +0100

    add response status personalisation

--- src/main/java/org/ouroui/interns/controller/InternController.java
+++ src/main/java/org/ouroui/interns/controller/InternController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.ouroui.interns.exception.ResourceNotFoundException;
 import org.ouroui.interns.model.Internship;
 import org.ouroui.interns.repository.InternsRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,41 +15,51 @@ import java.util.List;
 @RequestMapping("/api")
 public class InternController {
     InternsRepository internsRepository;
-    public InternController(InternsRepository internsRepository){
-        this.internsRepository=internsRepository;
+
+    public InternController(InternsRepository internsRepository) {
+        this.internsRepository = internsRepository;
     }
+
     @GetMapping("/Internships")
-    public List<Internship> getAllInternship(){
-        return internsRepository.findAll();
+    public ResponseEntity<List<Internship>> getAllInternship() {
+        return ResponseEntity.ok(internsRepository.findAll());
     }
+
     @PostMapping("/add")
-    public Internship add(@Valid @RequestBody Internship internship){
-        return internsRepository.save(internship);
+    public ResponseEntity<Internship> add(@Valid @RequestBody Internship internship) {
+        return new ResponseEntity<>(internsRepository.save(internship), HttpStatus.CREATED);
     }
+
     @GetMapping("/Internships/id/{id}")
-    public Internship getById(@PathVariable(value = "id") Long id){
-        return internsRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Internship","id",id));
+    public ResponseEntity<Internship> getById(@PathVariable(value = "id") Long id) {
+        return ResponseEntity.ok(internsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Internship", "id", id)));
     }
+
     @GetMapping("/Internships/role/{role}")
-    public List<Internship> getByRole(@PathVariable String role){
-        return internsRepository.findInternshipByRoleContaining(role); }
+    public ResponseEntity<List<Internship>> getByRole(@PathVariable String role) {
+        return ResponseEntity.ok(internsRepository.findInternshipByRoleContaining(role));
+    }
+
     @GetMapping("/Internships/duration/{duration}")
-    public List<Internship> getBydura(@PathVariable int duration){
-        return internsRepository.findInternshipByDuration(duration); }
+    public ResponseEntity<List<Internship>> getBydura(@PathVariable int duration) {
+        return ResponseEntity.ok(internsRepository.findInternshipByDuration(duration));
+    }
+
     @PutMapping("/Internships/put/{id}")
-    public Internship update(@PathVariable(value = "id") Long id,@Valid @RequestBody Internship internship){
-        Internship i=internsRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Internship","id",id));
+    public ResponseEntity<Internship> update(@PathVariable(value = "id") Long id, @Valid @RequestBody Internship internship) {
+        Internship i = internsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Internship", "id", id));
         i.setCompany(internship.getCompany());
         i.setRole(internship.getRole());
         i.setDuration(internship.getDuration());
         i.setLocation(internship.getLocation());
         i.setUrl(internship.getUrl());
-        return internsRepository.save(i);
+        return ResponseEntity.accepted().body(internsRepository.save(i));
     }
+
     @DeleteMapping("Internships/{id}")
-    public ResponseEntity<?> deleteCompany(@PathVariable(value = "id") long id){
-        Internship internship=internsRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Internship","id",id));
+    public ResponseEntity<?> deleteCompany(@PathVariable(value = "id") long id) {
+        Internship internship = internsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Internship", "id", id));
         internsRepository.delete(internship);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
