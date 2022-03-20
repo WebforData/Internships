07bcfab01c39bf8e6058a618ad79639cbe8e81bd
WebforData <kiraourui@gmail.com>
2022-03-20 22:40:15 +0100

    Commit (company api 2)

--- src/main/java/org/ouroui/interns/repository/CompanyRepository.java
+++ src/main/java/org/ouroui/interns/repository/CompanyRepository.java
@@ -4,7 +4,6 @@ import org.ouroui.interns.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company,Long> {
