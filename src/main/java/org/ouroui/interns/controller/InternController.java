6d492e83caeb42961073dcf224d67d1fbeebdb1d
WebforData <kiraourui@gmail.com>
2022-03-22 15:15:22 +0100

    Custom JSON Response with ResponseEntity

--- src/main/java/org/ouroui/interns/controller/InternController.java
+++ src/main/java/org/ouroui/interns/controller/InternController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.ouroui.interns.exception.ResourceNotFoundException;
 import org.ouroui.interns.model.Internship;
 import org.ouroui.interns.repository.InternsRepository;
+import org.ouroui.interns.responce.ResponceHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,57 +22,101 @@ public class InternController {
     }
 
     @GetMapping("/Internships")
-    public ResponseEntity<List<Internship>> getAllInternship() {
-        return ResponseEntity.ok(internsRepository.findAll());
+    public ResponseEntity<Object> getAllInternship() {
+        try {
+            return ResponceHandler.generateResponce("all internships", HttpStatus.OK, internsRepository.findAll());
+        } catch (Exception e) {
+            return ResponceHandler.generateResponce(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
     }
 
     @GetMapping("Internships/company/{company}")
-    public ResponseEntity<List<Internship>> findCompany(@PathVariable String company){
-        return ResponseEntity.ok(internsRepository.findByCompanyContaining(company));
+    public ResponseEntity<Object> findCompany(@PathVariable String company) {
+        try {
+            return ResponceHandler.generateResponce("get :" + company, HttpStatus.OK, internsRepository.findByCompanyContaining(company));
+        } catch (Exception e) {
+            return ResponceHandler.generateResponce(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
     }
+
     @PostMapping("/add")
-    public ResponseEntity<Internship> add(@Valid @RequestBody Internship internship) {
-        return new ResponseEntity<>(internsRepository.save(internship), HttpStatus.CREATED);
+    public ResponseEntity<Object> add(@Valid @RequestBody Internship internship) {
+        try {
+            return ResponceHandler.generateResponce("added successfully", HttpStatus.CREATED, internsRepository.save(internship));
+        } catch (Exception e) {
+            return ResponceHandler.generateResponce(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+        }
     }
 
     @GetMapping("/Internships/id/{id}")
-    public ResponseEntity<Internship> getById(@PathVariable(value = "id") Long id) {
-        return ResponseEntity.ok(internsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Internship", "id", id)));
+    public ResponseEntity<Object> getById(@PathVariable(value = "id") Long id) {
+        try {
+            return ResponceHandler.generateResponce("id: "+id,HttpStatus.OK,internsRepository.findById(id));
+        }catch (Exception e){
+            return ResponceHandler.generateResponce(e.getMessage(),HttpStatus.MULTI_STATUS,null);
+        }
     }
 
     @GetMapping("/Internships/role/{role}")
-    public ResponseEntity<List<Internship>> getByRole(@PathVariable String role) {
-        return ResponseEntity.ok(internsRepository.findInternshipByRoleContaining(role));
+    public ResponseEntity<Object> getByRole(@PathVariable String role) {
+    try {
+        return ResponceHandler.generateResponce("role: "+role,HttpStatus.OK,internsRepository.findInternshipByRoleContaining(role));
+    }catch (Exception e){
+        return ResponceHandler.generateResponce(e.getMessage(),HttpStatus.MULTI_STATUS,null);
+    }
     }
 
     @GetMapping("/Internships/duration/{duration}")
-    public ResponseEntity<List<Internship>> getBydura(@PathVariable int duration) {
-        return ResponseEntity.ok(internsRepository.findInternshipByDuration(duration));
+    public ResponseEntity<Object> getBydura(@PathVariable int duration) {
+        try {
+            return ResponceHandler.generateResponce("duration: "+duration,HttpStatus.OK,internsRepository.findInternshipByDuration(duration));
+        }catch (Exception e){
+            return ResponceHandler.generateResponce(e.getMessage(),HttpStatus.MULTI_STATUS,null);
+        }
     }
+
     @GetMapping("/Internships/duration/plus/{duration}")
-    public ResponseEntity<List<Internship>> getBydurationGreat(@PathVariable int duration) {
-        return ResponseEntity.ok(internsRepository.findByDurationGreaterThan(duration));
+    public ResponseEntity<Object> getBydurationGreat(@PathVariable int duration) {
+       try {
+           return ResponceHandler.generateResponce("dduratin great than : "+duration,HttpStatus.OK,internsRepository.findByDurationGreaterThan(duration));
+       }catch (Exception e){
+           return ResponceHandler.generateResponce(e.getMessage(),HttpStatus.MULTI_STATUS,null);
+       }
     }
+
     @GetMapping("/Internships/duration/less/{duration}")
-    public ResponseEntity<List<Internship>> getBydurationLess(@PathVariable int duration) {
-        return ResponseEntity.ok(internsRepository.findByDurationLessThan(duration));
+    public ResponseEntity<Object> getBydurationLess(@PathVariable int duration) {
+        try {
+            return ResponceHandler.generateResponce("dduratin less than : "+duration,HttpStatus.OK,internsRepository.findByDurationLessThan(duration));
+        }catch (Exception e){
+            return ResponceHandler.generateResponce(e.getMessage(),HttpStatus.MULTI_STATUS,null);
+        }
     }
 
     @PutMapping("/Internships/put/{id}")
-    public ResponseEntity<Internship> update(@PathVariable(value = "id") Long id, @Valid @RequestBody Internship internship) {
+    public ResponseEntity<Object> update(@PathVariable(value = "id") Long id, @Valid @RequestBody Internship internship) {
         Internship i = internsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Internship", "id", id));
         i.setCompany(internship.getCompany());
         i.setRole(internship.getRole());
         i.setDuration(internship.getDuration());
         i.setLocation(internship.getLocation());
         i.setUrl(internship.getUrl());
-        return ResponseEntity.accepted().body(internsRepository.save(i));
+       try {
+           return ResponceHandler.generateResponce("modified successfully",HttpStatus.OK,internsRepository.save(i));
+       }catch (Exception e){
+           return ResponceHandler.generateResponce(e.getMessage(),HttpStatus.MULTI_STATUS,null);
+       }
     }
 
     @DeleteMapping("Internships/{id}")
     public ResponseEntity<?> deleteCompany(@PathVariable(value = "id") long id) {
         Internship internship = internsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Internship", "id", id));
-        internsRepository.delete(internship);
-        return ResponseEntity.noContent().build();
+        try{
+            internsRepository.delete(internship);
+            return ResponceHandler.generateResponce("Deleted!!",HttpStatus.OK,id+" deleted");
+        }catch (Exception e){
+            return ResponceHandler.generateResponce(e.getMessage(),HttpStatus.MULTI_STATUS,null);
+        }
+
     }
 }
--- src/main/java/org/ouroui/interns/repository/CompanyRepository.java
+++ src/main/java/org/ouroui/interns/repository/CompanyRepository.java
@@ -12,8 +12,7 @@ import java.util.List;
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     Company findById(long id);
-
-    //Get comapany name with the role and location of internship associated
+    //Get comapany name with the role of internship associated
     @Query("select new org.ouroui.interns.Dto.OrderResponce(c.Name, i.role) from Company c join c.internshipList i")
     List<OrderResponce> getJoinInfo();
 }
